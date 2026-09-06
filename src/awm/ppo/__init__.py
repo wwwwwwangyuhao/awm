ec0ccafd54387c3789b3e452c7487371bb00c862
wwwwwwangyuhao <101957641+wwwwwwangyuhao@users.noreply.github.com>
2026-09-06 19:24:34 +0800

    Expose PPO rollout collectors

--- src/awm/ppo/__init__.py
+++ src/awm/ppo/__init__.py
@@ -5,15 +5,23 @@ from .buffer import PPORolloutBatch, PPORolloutBuffer
 from .models import HierarchicalActionBatch, HierarchicalIrrigationActor, IrrigationValueNetwork
 from .normalization import NormalizerState, RunningObservationNormalizer
 from .reward import POLICY_BUDGET_MM, PPOEpisodeReward, PPORewardBreakdown
+from .rollout import (
+    BalancedRolloutResult,
+    PPOEpisodeOutcome,
+    collect_balanced_training_rollout,
+    collect_episode,
+)
 from .scheduler import WeatherEtaCell, balanced_training_cycle, training_cells, validation_cells
 
 __all__ = [
+    "BalancedRolloutResult",
     "HierarchicalActionBatch",
     "HierarchicalIrrigationActor",
     "IrrigationValueNetwork",
     "NormalizerState",
     "POLICY_BUDGET_MM",
     "PPOAgent",
+    "PPOEpisodeOutcome",
     "PPOEpisodeReward",
     "PPOHyperparameters",
     "PPORolloutBatch",
@@ -23,6 +31,8 @@ __all__ = [
     "RunningObservationNormalizer",
     "WeatherEtaCell",
     "balanced_training_cycle",
+    "collect_balanced_training_rollout",
+    "collect_episode",
     "training_cells",
     "validation_cells",
 ]
